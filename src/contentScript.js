31317d547797c13268d682576674866e0494d605
DepthPixels <depthclawclashedor@gmail.com>
2025-07-18 17:30:53 +0530

    Adapt to Government sites
    element ID's changed to fit the government site. First production model ready.

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,16 +1,16 @@
 import { supabase } from "./supabaseClient.js";
 
-const patient_id = await document.getElementById("patient_id").value;
+const patient_id = await document.getElementById("ctl00_ContentPlaceHolder1_txtPatientRegNo").value;
 
 let { data, error } = await supabase.from("patients").select("*").eq("patient_id", patient_id);
 
-document.getElementById("date_of_usg").value = data[0].date_of_usg;
-document.getElementById("patient_name").value = data[0].patient_name;
-document.getElementById("husband_name").value = data[0].husband_name;
-document.getElementById("patient_age").value = data[0].patient_age;
-document.getElementById("last_menstrual_period").value = data[0].last_menstrual_period;
-document.getElementById("number_of_male_children").value = data[0].number_of_male_children;
-document.getElementById("number_of_female_children").value = data[0].number_of_female_children;
-document.getElementById("referred_by").value = data[0].referred_by;
-document.getElementById("mobile_no").value = data[0].mobile_no;
-document.getElementById("address").value = data[0].address;
+document.getElementById("ctl00_ContentPlaceHolder1_txtPRegDate").value = data[0].date_of_usg;
+document.getElementById("ctl00_ContentPlaceHolder1_txtPFirstName").value = data[0].patient_name;
+document.getElementById("ctl00_ContentPlaceHolder1_rbtnHusbWFMOf_0").checked = true;
+document.getElementById("ctl00_ContentPlaceHolder1_txtHusbandName").value = data[0].husband_name;
+document.getElementById("ctl00_ContentPlaceHolder1_txtPAge").value = data[0].patient_age;
+document.getElementById("ctl00_ContentPlaceHolder1_txtLMPDate").value = data[0].last_menstrual_period;
+document.getElementById("ctl00_ContentPlaceHolder1_txtMale").value = data[0].number_of_male_children;
+document.getElementById("ctl00_ContentPlaceHolder1_txtFemale").value = data[0].number_of_female_children;
+document.getElementById("ctl00_ContentPlaceHolder1_txtMobileNo").value = data[0].mobile_no;
+document.getElementById("ctl00_ContentPlaceHolder1_txtPatientAdd").value = data[0].address;
